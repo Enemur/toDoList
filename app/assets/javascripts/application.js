67a8e5d898924fc797fc18ad849bb731fecafb54
pavel <pavelametov@gmail.com>
2019-01-14 22:56:45 +0300

    add some styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,17 +12,21 @@
 //
 //= require rails-ujs
 //= require jquery
-//= require jquery.min.js
+//= require vendor/iCheck/icheck.js
 //= require_tree .
+//= require select2
 
 $(document).ready(function() {
-    $("#create_to_do").on("click", function (event) {
+    $("#create-to-do").on("click", function (event) {
         event.preventDefault();
-
-        $("#form_create_to_do").submit();
+        let project_id = $('.form-select-project').val();
+        let text = $(".text-field-form").val();
+        if (project_id > 0 && text !== '') {
+            $("#form_create_to_do").submit();
+        }
     });
 
-    $("#hide_create_to_do").on("click", function (event) {
+    $("#hide-create-to-do").on("click", function (event) {
         event.preventDefault();
 
         $("#div_create_to_do").hide();
@@ -32,5 +36,27 @@ $(document).ready(function() {
         event.preventDefault();
 
         $("#div_create_to_do").show();
+    });
+
+    $('input')
+        .iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square-blue'
+        })
+        .on('ifToggled', function (event) {
+            let isChecked = $(this).is(':checked');
+            let val = $(this).val();
+
+            if (isChecked)
+                $('#li-'+val).css("text-decoration", "line-through");
+            else
+                $('#li-'+val).css("text-decoration", "none");
+
+            $('#form-'+val).submit();
+        });
+
+    $('.form-select-project').select2({
+        minimumResultsForSearch: -1,
+        placeholder: "Категория"
     });
 });
--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -1,37 +1,28 @@
 class ToDosController < ApplicationController
   def create
-    @project = Project.find(params[:to_do][:project_id])
+    @project = Project.find(params[:project_id])
     @to_do = @project.to_dos.create(to_do_params)
 
     redirect_to projects_index_path
   end
 
   def index
-    @project = Project.find(params[:to_do][:project_id])
+    @project = Project.find(params[:project_id])
     @to_dos = @project.to_dos
 
     redirect_to projects_index_path
   end
 
   def update
-    @project = Project.find(params[:to_do][:project_id])
-    @to_do = @project.to_dos.find(params[:id])
+    @to_do = ToDo.find(params[:id])
 
-    @to_do.update(to_do_params)
-
-    redirect_to projects_index_path
-  end
-
-  def destroy
-    @project = Project.find(params[:to_do][:project_id])
-    @to_do = @project.to_dos.find(params[:id])
-    @to_do.destroy
+    @to_do.update(isCompleted: params[:isCompleted])
 
     redirect_to projects_index_path
   end
 
   private
     def to_do_params
-      params.require(:to_do).permit(:text, :isCompleted, :project_id)
+      params.require(:to_do).permit(:text, :isCompleted)
     end
 end
